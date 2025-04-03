1d7528b65f005d67f43a75325ba5d3f654c56180
Avin-Arunkumar <avinarun2005@gmail.com>
2025-04-03 22:00:11 +0530

    Title update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const Port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Welcome to ShopEasy!");
+  res.send("Welcome to ShopEasy testing !");
 });
 app.listen(Port, () => {
   console.log(`The port is running on ${Port}`);
